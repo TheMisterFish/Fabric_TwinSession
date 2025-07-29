1fa63d284c2ec1c002d86eeabfb307517d4ff77b
Fish <vincentvenhuizen1997@gmail.com>
2025-07-29 22:43:29 +0200

    Remove unused imports

--- src/main/java/com/twinsession/mixin/PlayerList_TwinSessionMixin.java
+++ src/main/java/com/twinsession/mixin/PlayerList_TwinSessionMixin.java
@@ -1,7 +1,6 @@
 package com.twinsession.mixin;
 
 import com.twinsession.TwinSession;
-import com.mojang.authlib.GameProfile;
 import net.minecraft.network.Connection;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.players.PlayerList;
@@ -9,7 +8,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerList.class)
 public class PlayerList_TwinSessionMixin {
